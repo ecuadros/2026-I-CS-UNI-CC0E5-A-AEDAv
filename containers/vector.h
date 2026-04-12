209f6626582aae8afd40286ce34fcb3ff7cda117
r0ger <franklin.espinoza0226@gmail.com>
2026-04-12 17:57:29 -0500

    feat: add mutex for thread-safe ForEach and ReverseForEach methods in Vector

--- containers/vector.h
+++ containers/vector.h
@@ -50,7 +50,7 @@ public:
     void setData(T data) { m_data = data; }
     Ref  getRef() { return m_ref; }
     void setRef(Ref ref) { m_ref = ref; }
-    
+
 };
 
 template <typename T>
@@ -86,16 +86,16 @@ public:
 
     backward_iterator rbegin() { return backward_iterator(this, m_data + m_size - 1); }
     backward_iterator rend()   { return backward_iterator(this, m_data - 1); }
-    
-    // TODO: Agregar control concurrente
+
     template <typename Func, typename... Args>
     void ForEach(Func func, Args &&...  args){
+        scoped_lock lock(m_mtx);
         ::ForEach(begin(), end(), func, std::forward<Args>(args)... );
     }
 
-    // TODO: Agregar control concurrente
     template <typename Func, typename... Args>
     void ReverseForEach(Func func, Args &&...  args){
+        scoped_lock lock(m_mtx);
         ::ForEach(rbegin(), rend(), func, std::forward<Args>(args)... );
     }
 };
