2756cbeaf9a023d0971d782c777ce24ff6099a3d
frankhz28 <frank.hinojosa.z@uni.pe>
2026-04-13 20:34:06 -0500

    Agregar control concurrente ForEach y ReverseForEach

--- containers/vector.h
+++ containers/vector.h
@@ -90,12 +90,14 @@ public:
     // TODO: Agregar control concurrente
     template <typename Func, typename... Args>
     void ForEach(Func func, Args &&...  args){
+        scoped_lock lock(m_mtx);
         ::ForEach(begin(), end(), func, std::forward<Args>(args)... );
     }
 
     // TODO: Agregar control concurrente
     template <typename Func, typename... Args>
     void ReverseForEach(Func func, Args &&...  args){
+        scoped_lock lock(m_mtx);
         ::ForEach(rbegin(), rend(), func, std::forward<Args>(args)... );
     }
 };
