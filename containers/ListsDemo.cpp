724b1726bfc7153e38e558f3cb6a6bd6cfac516b
frankhz28 <frank.hinojosa.z@uni.pe>
2026-05-19 23:04:05 -0500

    Actividad 1 - Reutilizacion

--- containers/ListsDemo.cpp
+++ containers/ListsDemo.cpp
@@ -1,111 +1,158 @@
 #include <iostream>
-#include <fstream>
-#include <string>
-
-#include "../types.h"
 #include "linkedlist.h"
-// #include "doublelinkedlist.h"
-// #include "circularlinkedlist.h"
-// #include "circularlinkedlist.h"
+#include "doublelinkedlist.h"
+#include "circularlinkedlist.h"
+#include "circulardoublelinkedlist.h"
+#include "traits.h"
+#include "../types.h"
 
 using namespace std;
 
+// LinkedList Demo
 template <typename Container>
-void DemoList(Container& list, string fileName){
-    list.insert(28, 15);
-    list.insert(17, 25);
-    list.insert(8, 35);
-    list.insert(4, 45);
-    list.insert(35, 55);
-    cout << list << endl;
-    // Grabar la lista en un archivo
-    ofstream os(fileName);
-    os << list << endl;
+void DemoLinkedList(Container& list, const string& name) {    
+    // Agregar múltiples elementos
+    cout << "\n1. Insertando 5 elementos con push_back:" << endl;
+    list.push_back(50, 10);
+    list.push_back(30, 20);
+    list.push_back(70, 30);
+    list.push_back(20, 40);
+    list.push_back(80, 50);
+    cout << "   Tamaño: " << list.size() << endl;
     
-    // Leer la lista desde un archivo
-    ifstream is(fileName);
-    is >> list;
-    cout << list << endl;
-}
-
-void LinkedListDemo(){
-    LinkedList<T1, AscendingLinkedListTrait<T1>> list;
-    DemoList(list, "AscLL.txt");
-    LinkedList<T1, DescendingLinkedListTrait<T1>> list2;
-    DemoList(list2, "DescLL.txt");
-}
-
-void DoubleLinkedListDemo(){
-    // DoubleLinkedList<T1, AscendingDLLTrait<T1>> list;
-    // DemoList(list, "AscDLL.txt");
-    // DoubleLinkedList<T1, DescendingDLLTrait<T1>> list2;
-    // DemoList(list2, "DescDLL.txt");
-}
-
-void CircularLinkedListDemo(){
+    // Remover del frente
+    cout << "2. Pop front :" << endl;
+    auto [data, ref] = list.pop_front();
+    cout << "   Removido: (" << data << ", " << ref << ")" << endl;
+    cout << "   Tamaño: " << list.size() << endl;
     
-}
-
-void CircularLinkedListDemo(){
+    // Insertar elemento
+    cout << "3. Insertando elemento 60 con insert():" << endl;
+    list.insert(60, 60);
+    cout << "   Tamaño: " << list.size() << endl;
     
+    // Pop front nuevamente
+    cout << "4. Pop front adicional:" << endl;
+    auto [data2, ref2] = list.pop_front();
+    cout << "   Removido: (" << data2 << ", " << ref2 << ")" << endl;
+    cout << "   Tamaño final: " << list.size() << endl;
 }
 
-void ListsDemo(){
-    LinkedListDemo();
-    CircularLinkedListDemo();
-    DoubleLinkedListDemo();
-    CircularDoubleLinkedListDemo();
-}
-
-void TestConcurrencia() {
-    cout << "\nTEST DE CONCURRENCIA" << endl;
-    LinkedList<AscendingLinkedListTrait<T1>> list;
-
-    // 5 hilos van a intentar meter 1000 elementos cada uno al mismo tiempo
-    auto worker = [&list](int thread_id) {
-        for(int i = 0; i < 1000; i++) {
-            list.push_front(i, thread_id);
-        }
-    };
-
-    thread t1(worker, 1);
-    thread t2(worker, 2);
-    thread t3(worker, 3);
-    thread t4(worker, 4);
-    thread t5(worker, 5);
-
-    t1.join(); t2.join(); t3.join(); t4.join(); t5.join();
-
-    cout << "Se lanzaron 5 hilos insertando 1000 elementos simultaneamente." << endl;
-    cout << "Tamano de la lista (Esperado 5000): " << list.size() << endl;
-    if(list.size() == 5000) {
-        cout << "ESTADO: EXITO - El shared_mutex previno condiciones de carrera perfectamente." << endl;
-    } else {
-        cout << "ESTADO: FALLO - Hubo corrupcion de memoria." << endl;
-    }
-}
-void TestOperators() {
-    cout << "\nTEST DE OPERADORES" << endl;
-    LinkedList<AscendingLinkedListTrait<T1>> list;
+// DoubleLinkedList Demo
+template <typename Container>
+void DemoDoubleLinkedList(Container& dll, const string& name) {
+    // Agregar por atrás
+    cout << "\n1. Insertando 3 elementos con push_back:" << endl;
+    dll.push_back(50, 10);
+    dll.push_back(30, 20);
+    dll.push_back(70, 30);
+    cout << "   Tamaño: " << dll.size() << endl;
     
-    // 1. Probamos operator>> (Lectura)
-    cout << "Simulando lectura desde formato: [(10, 100), (20, 200), (30, 300)]" << endl;
-    stringstream simulador_input("[(10, 100), (20, 200), (30, 300)]");
-    simulador_input >> list;
-
-    // 2. Probamos operator<< (Escritura)
-    cout << "Lista luego de la lectura (operator<<): " << list << endl;
+    // Remover por atrás
+    cout << "2. Pop back (removiendo por el final):" << endl;
+    auto [data1, ref1] = dll.pop_back();
+    cout << "   Removido: (" << data1 << ", " << ref1 << ")" << endl;
     
-    // 3. Probamos operator[] (Acceso seguro por indice)
-    cout << "Accediendo al indice [0] (operator[]): Dato -> " << list[0] << endl;
-    cout << "Accediendo al indice [2] (operator[]): Dato -> " << list[2] << endl;
+    // Agregar por adelante
+    cout << "3. Agregando elemento por push_front:" << endl;
+    dll.push_front(100, 100);
+    cout << "   Tamaño: " << dll.size() << endl;
     
-    // Probamos la excepcion del operator[] (Descomentar para probar)
-    // cout << "Probando fuera de rango: " << list[5] << endl; // Lanzara la excepcion
+    // Remover del frente
+    cout << "4. Pop front:" << endl;
+    auto [data2, ref2] = dll.pop_front();
+    cout << "   Removido: (" << data2 << ", " << ref2 << ")" << endl;
+    cout << "   Tamaño final: " << dll.size() << endl;
+    
+    // Insertar con orden
+    cout << "5. Insertando elemento 40:" << endl;
+    dll.insert(40, 40);
+    cout << "   Tamaño: " << dll.size() << endl;
 }
-void ListsDemo(){
-    TestBasicos();
-    TestConcurrencia();
-    TestOperators();
-    cout << "\n=== FIN DE LAS PRUEBAS ===" << endl;
+
+// CircularLinkedList Demo
+template <typename Container>
+void DemoCircularLinkedList(Container& cll, const string& name) {
+    // Agregar elementos
+    cout << "\n1. Agregando elementos (circular):" << endl;
+    cll.push_back(10, 1);
+    cll.push_back(20, 2);
+    cll.push_back(30, 3);
+    cll.push_back(40, 4);
+    cout << "   Tamaño: " << cll.size() << endl;
+    cout << "   (Estructura circular: último → primero)" << endl;
+    
+    // Agregar al frente
+    cout << "2. Agregando al frente con push_front:" << endl;
+    cll.push_front(5, 5);
+    cout << "   Tamaño: " << cll.size() << endl;
+    
+    // Remover del frente
+    cout << "3. Pop front:" << endl;
+    auto [data, ref] = cll.pop_front();
+    cout << "   Removido: (" << data << ", " << ref << ")" << endl;
+    
+    // Insertar con orden
+    cout << "4. Insertando elemento 25:" << endl;
+    cll.insert(25, 25);
+    cout << "   Tamaño final: " << cll.size() << endl;
+}
+
+// CircularDoubleLinkedList Demo
+template <typename Container>
+void DemoCircularDoubleLinkedList(Container& cdll, const string& name) {
+    
+    // Agregar elementos
+    cout << "\n1. Agregando elementos con push_back:" << endl;
+    cdll.push_back(15, 1);
+    cdll.push_back(25, 2);
+    cdll.push_back(35, 3);
+    cout << "   Tamaño: " << cdll.size() << endl;
+    
+    // Push front
+    cout << "2. Agregando al frente con push_front:" << endl;
+    cdll.push_front(5, 5);
+    cout << "   Tamaño: " << cdll.size() << endl;
+    
+    // Pop back
+    cout << "3. Pop back (removiendo del final):" << endl;
+    auto [data1, ref1] = cdll.pop_back();
+    cout << "   Removido: (" << data1 << ", " << ref1 << ")" << endl;
+    
+    // Pop front
+    cout << "4. Pop front:" << endl;
+    auto [data2, ref2] = cdll.pop_front();
+    cout << "   Removido: (" << data2 << ", " << ref2 << ")" << endl;
+    
+    // Insert
+    cout << "5. Insertando 30:" << endl;
+    cdll.insert(30, 30);
+    cout << "   Tamaño final: " << cdll.size() << endl;
+    cout << "   (Estructura bidireccional circular)" << endl;
+}
+
+void ListsDemo() {   
+    cout << "LINKED LISTS" << endl;
+    LinkedList<AscendingTrait<LLNode<T1>>> ll;
+    DemoLinkedList(ll, "LinkedList Ascendente");
+    LinkedList<DescendingTrait<LLNode<T1>>> ll_desc;
+    DemoLinkedList(ll_desc, "LinkedList Descendente");
+
+    cout << "DOUBLE LINKED LISTS" << endl;    
+    DoubleLinkedList<AscendingTrait<DLLNode<T1>>> dll;
+    DemoDoubleLinkedList(dll, "DoubleLinkedList Ascendente");
+    DoubleLinkedList<DescendingTrait<DLLNode<T1>>> dll_desc;
+    DemoDoubleLinkedList(dll_desc, "DoubleLinkedList Descendente");
+    
+    cout << "CIRCULAR LINKED LISTS║" << endl;    
+    CircularLinkedList<AscendingTrait<LLNode<T1>>> cll;
+    DemoCircularLinkedList(cll, "CircularLinkedList Ascendente");
+    CircularLinkedList<DescendingTrait<LLNode<T1>>> cll_desc;
+    DemoCircularLinkedList(cll_desc, "CircularLinkedList Descendente");
+    
+    cout << "CIRCULAR DOUBLE LISTS" << endl;
+    CircularDoubleLinkedList<AscendingTrait<DLLNode<T1>>> cdll;
+    DemoCircularDoubleLinkedList(cdll, "CircularDoubleLinkedList Ascendente");
+    CircularDoubleLinkedList<DescendingTrait<DLLNode<T1>>> cdll_desc;
+    DemoCircularDoubleLinkedList(cdll_desc, "CircularDoubleLinkedList Descendente");
 }
